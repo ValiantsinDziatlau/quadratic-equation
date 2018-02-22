e9c077e019eca831c5f754e4f685c7f4898c4071
ValiantsinDziatlau <valiantsindziatlau@gmail.com>
2018-02-23 00:42:21 +0300

    simple

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let a,b,c;
+    let Arr=[];
+    a= Number(equation.slice(0, equation.indexOf(" * x^2")).replace(" ",""));
+    b= Number(equation.slice(equation.indexOf(" * x^2 ")+7, equation.indexOf(" * x ")).replace(" ",""));
+    c= Number(equation.slice(equation.indexOf(" * x ")+5, equation.length).replace(" ",""));
+    
+    if(a!==0&&b^2>4*a*c){
+        Arr[0]=Math.round((-b+Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
+        Arr[1]=Math.round((-b-Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a));
+        return Arr.sort((left,right)=> left-right);
+    }
+    else{
+        return undefined;
+    }
 }
